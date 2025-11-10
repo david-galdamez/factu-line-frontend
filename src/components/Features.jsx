b2ee1ca6df4778c97adfa0ad3255966fce77912f
Diego Hurtado <164244994+diegohurtado925@users.noreply.github.com>
2025-11-10 14:34:35 -0600

    feat: Mejorar fetures

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,22 +1,53 @@
-import React from 'react';
+import imgFacturas from '/assets/feature-facturas.png';
+import imgClientes from '/assets/feature-clientes.png';
+import imgEstadisticas from '/assets/feature-estadisticas.png';
+
 
 const featuresList = [
-  { title: 'Generación rápida de facturas' },
-  { title: 'Gestión de clientes y productos' },
-  { title: 'Informe y estadísticas' }
+  { 
+    title: 'Generación rápida de facturas', 
+    image: imgFacturas,
+    description: 'Crea facturas profesionales en segundos y envíalas automáticamente a tus clientes.'
+  },
+  { 
+    title: 'Gestión de clientes y productos', 
+    image: imgClientes ,
+    description: 'Mantén tu base de datos organizada y accede rápidamente a la información de tus contactos.'
+  },
+  { 
+    title: 'Informe y estadísticas', 
+    image: imgEstadisticas,
+    description: 'Obtén información valiosa sobre el rendimiento de tu negocio con informes detallados.'
+  }
 ];
 
-
 function Features() {
+  
   return (
+    
     <section className="features">
       <h2>¿Cómo simplificamos tu negocio?</h2>
+
+
       <div className="features-container">
         {featuresList.map((feature, index) => (
-          <div className="feature-card" key={index}>
-            <p>{feature.title}</p>
-            <div className="image-placeholder">
-              <span>Imagen</span>
+          <div className="card" key={index}>
+            <div className='card-inner'>
+              <div className="card-front">
+                <div className="image-container">
+                                  <p>{feature.title}</p>
+
+                  <img 
+                    src={feature.image} 
+                    alt={feature.title} 
+                    className="feature-img" 
+                  />
+                </div>
+              </div>
+
+              <div className="card-back">
+              <p>{feature.description}</p>
+              </div>
             </div>
           </div>
         ))}
